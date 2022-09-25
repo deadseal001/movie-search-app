f56b6b28de667e733416f73665ea0fa3e6568fd9
mrdekraker <30578794+mrdekraker@users.noreply.github.com>
2022-09-25 09:10:11 -0500

    added youtube iframe

--- assets/js/trailers.js
+++ assets/js/trailers.js
@@ -2,13 +2,14 @@ const searchInput = $(`.search-box`);
 const apiKey = `a725cdae9e4df8f603b55513ee6ac4e8`;
 let movieID = [];
 let movieTitle = [];
+let movieDate = [];
 let moviePoster = [];
 let movieTrailerKey = [];
 
 const displayMovieData = function () {
   // MOVIE TITLE
   const movieTitleEl = $(`.movie-title`);
-  $(movieTitleEl).text(movieTitle);
+  $(movieTitleEl).text(`${movieTitle}, Released: ${movieDate}`);
 
   // MOVIE POSTER
   const moviePosterEl = $(`#movie-poster`);
@@ -16,22 +17,27 @@ const displayMovieData = function () {
 
   // MOVIE TRAILER
   const movieTrailerEl = $(`#movie-trailer`);
-  const movieTrailerFrame = $(`iframe`);
-  $(movieTrailerFrame).attr(`src`, `https://www.youtube.com/embed/${movieTrailerKey}`);
+  const movieTrailerFrame = document.createElement(`iframe`);
+  $(movieTrailerFrame).attr({
+    src: `https://www.youtube.com/embed/${movieTrailerKey}`,
+    frameborder: `0`,
+    width: `560`,
+    height: `360`,
+  });
 
   $(movieTrailerEl).append(movieTrailerFrame);
 };
 
 const getMovieData = async (movieSearch) => {
-  const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${movieSearch}`;
-
   // FETCH MOVIE DATA
+  const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${movieSearch}`;
   const movieData = await fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => data);
-  // MOVIE ID, TITLE, POSTER
+  // MOVIE ID, TITLE, DATE RELEASED, POSTER
   movieID = movieData.results[0].id;
   movieTitle = movieData.results[0].title;
+  movieDate = movieData.results[0].release_date;
   moviePoster = movieData.results[0].poster_path;
 
   // FETCH MOVIE TRAILER
